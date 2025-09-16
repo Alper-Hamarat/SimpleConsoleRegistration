4fa19bb883cfde91ef0c62e7d740d9f795d0e919
Alper <47501174+AlperGit@users.noreply.github.com>
2025-09-16 13:46:41 +0200

    Updated Readme for Module breakdown

---
+++ src/menu.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#include "../include/menu.hpp"
+
+using namespace std;
+
+void printMenu()
+{
+    cout << "Hello Would you like to login or register?" << endl;
+    cout << "0: to registration" << endl;
+    cout << "1: to login" << endl;
+    cout << "9: to exit" << endl;
+}
